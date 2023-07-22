fa947edbdb026af88b646cf60ec8198229c110f3
Souhail AIT LAHCEN <55179344+Souhail99@users.noreply.github.com>
2023-07-22 11:57:13 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             <p className={inter.className}>
               Test our Solution !
             </p>
-        
+          <Link/>
 
           <Link
             href="/doc"
