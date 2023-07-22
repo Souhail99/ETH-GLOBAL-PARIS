96de2ce44b4456270e8433028de29c836f54c89f
lilyanB <l.bastien@massa.net>
2023-07-22 18:14:44 +0200

    add verifiyer

--- components/buttons/SismoConnectButton.tsx
+++ components/buttons/SismoConnectButton.tsx
@@ -6,54 +6,34 @@ import {
   SismoConnectButton,
   AuthType,
   SismoConnectResponse,
-  ClaimType,
-  SismoConnect,
-  SismoConnectConfig,
 } from "@sismo-core/sismo-connect-react";
 
 export const SismoConnectComponent = () => {
-  type VaultConfig = {
-    impersonate: string[];
-  };
-
   const appId: string = "0xedae8cc49b4f32e436691771aadd5393";
-  const config: SismoConnectConfig = {
-    appId: appId,
-  };
-
-  //const sismoConnect = SismoConnect({config});
 
   return (
     <SismoConnectButton
       config={{
         appId: appId, // replace with your appId
-        // displayRawResponse: true,
       }}
-      // request proof of Data Sources ownership (e.g EVM, GitHub, twitter or telegram)
-      auths={[{ authType: AuthType.EVM_ACCOUNT }]}
-      // request zk proof that Data Source are part of a group
-      // (e.g NFT ownership, Dao Participation, GitHub commits)
-      claims={[]}
-      // request message signature from users.
-      signature={{ message: "I vote Yes to Privacy" }}
-      // retrieve the Sismo Connect Reponse from the user's Sismo data vault
+      auths={[{ authType: AuthType.VAULT }]}
+      claims={[{ groupId: "0x9b72562239c38dbc6fe8a0ff443019bf" }]}
+      signature={{ message: "0x00" }}
       onResponse={async (response: SismoConnectResponse) => {
         console.log(response);
-        const res1 = await fetch("/api/hello", {
+        const endpoint = "api/verifiyer";
+        const data = {
+          proof: response,
+        };
+        // Send the data to the server in JSON format.
+        const options = {
           method: "POST",
-          body: JSON.stringify(response),
-        });
-        console.log(await res1.json());
-        const res = await fetch("/api/verify", {
-          method: "POST",
-          body: JSON.stringify(response),
-        });
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        };
+        const res = await fetch(endpoint, options);
         console.log(await res.json());
       }}
-      // reponse in bytes to call a contract
-      // onResponseBytes={async (response: string) => {
-      //   console.log(response);
-      // }}
     />
   );
 };
--- pages/api/verifiyer.ts
+++ pages/api/verifiyer.ts
@@ -1,38 +1,41 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  SismoConnectConfig,
+  SismoConnectResponse,
+} from "@sismo-core/sismo-connect-react";
 import {
   SismoConnect,
   SismoConnectVerifiedResult,
   AuthType,
 } from "@sismo-core/sismo-connect-server";
-import {
-  SismoConnectConfig,
-  SismoConnectResponse,
-} from "@sismo-core/sismo-connect-react";
-
-type Data = {
-  name: string;
-};
 
 export default async function handler(req: any, res: any) {
   if (req.method === "POST") {
-    // Process a POST request
-    // const sismoConnectResponse: SismoConnectResponse = req.body;
-    // const appId: string = "0xedae8cc49b4f32e436691771aadd5393";
-    // const config: SismoConnectConfig = {
-    //   appId: appId,
-    // };
-    // const sismoConnect = SismoConnect({ config });
-    // // verifies the proofs contained in the sismoConnectResponse
-    // const result: SismoConnectVerifiedResult = await sismoConnect.verify(
-    //   sismoConnectResponse,
-    //   {
-    //     // signature request must be made with at least an auth or one claim
-    //     auths: [{ authType: AuthType.EVM_ACCOUNT }],
-    //     // your signature request
-    //     signature: { message: "0x00" },
-    //   }
-    // );
+    const appId: string = "0xedae8cc49b4f32e436691771aadd5393";
+    const config: SismoConnectConfig = {
+      appId: appId,
+    };
+
+    const sismoConnect = SismoConnect({ config });
+
+    async function verifyResponse(sismoConnectResponse: SismoConnectResponse) {
+      // verifies the proofs contained in the sismoConnectResponse
+      const result: SismoConnectVerifiedResult = await sismoConnect.verify(
+        sismoConnectResponse,
+        {
+          // request proof of Data Sources ownership (e.g EVM, GitHub, twitter or telegram)
+          auths: [{ authType: AuthType.VAULT }],
+          // request zk proof that Data Source are part of a group
+          // (e.g NFT ownership, Dao Participation, GitHub commits)
+          claims: [{ groupId: "0x9b72562239c38dbc6fe8a0ff443019bf" }],
+          // request message signature from users.
+          signature: { message: "0x00" },
+        }
+      );
+      return result;
+    }
+    const result = await verifyResponse(req.body.proof);
+    const vaultId = result.getUserId(AuthType.VAULT);
+    res.status(200).json({ all: result, vaultId: vaultId });
   } else {
     res.status(200).json({ name: "edrfgthyj" });
   }
